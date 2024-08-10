decd01beba8fa668ca3a7a644c45709252a66dba
pb01146 <prakashkumar.behera@randstad.com>
2024-08-10 10:33:03 +0200

    changed the output format

--- first_script.py
+++ first_script.py
@@ -4,4 +4,5 @@ aws_mgmt_console = boto3.session.Session(profile_name="kpb.devops")
 iam_console = aws_mgmt_console.resource("iam")
 
 for each_user in iam_console.users.all():
-    print(each_user.name)
+    print("User Name: ", each_user.name)
+
